7698789715006bc8d1ae535abde57ab7ae641bf8
Rifqi Ali Abdullah <rifqialiabdullah@gmail.com>
2024-11-20 10:58:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.bottomnavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.background = null
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.check -> replaceFragment(CheckFragment())
+                R.id.block -> replaceFragment(BlockFragment())
+                R.id.accesstime -> replaceFragment(AccessTimeFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
